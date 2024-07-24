446f06f5e5bf0a4a0a7b34076d33ecd30778e6b6
AfrodytaP <59373204+AfrodytaP@users.noreply.github.com>
2024-07-24 21:32:11 +0100

    matrix

--- frontend/src/app/matrix/page.tsx
+++ frontend/src/app/matrix/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ScatterPlot from "@/components/ScatterPlot";
+import BusinessValueInfo from "@/components/BusinessValueInfo";
 import { useState } from "react";
 import withAuth from "@/hoc/withAuth";
 
@@ -62,6 +63,7 @@ const Matrix = () => {
     <main className="ml-44 mr-44 mt-24">
       <section className="h-screen flex flex-col items-center">
         <ScatterPlot data={data} />
+        <BusinessValueInfo />
       </section>
     </main>
   );
